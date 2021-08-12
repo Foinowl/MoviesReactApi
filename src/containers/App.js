b556959d8621ed10ecb716e1514b004fe84701c7
Evgeniy <nifpopnaf@gmail.com>
2021-08-12 16:49:29 +0400

    layouted main blocks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from "react-redux"
+import React from "react"
+import styled from "styled-components"
 
-import styled from 'styled-components';
+import Sidebar from "../components/sidebar/Sidebar"
+import Header from "../components/header/Header"
 
 
+const MainWrapper = styled.div`
+	display: flex;
+`
+
+const ContentWrapper = styled.div`
+	width: 100%;
+`
 
 const App = () => {
-	<h1>App </h1>
+	return (
+		<React.Fragment>
+			<MainWrapper>
+				<Sidebar />
+				<ContentWrapper>
+					<Header />
+				</ContentWrapper>
+			</MainWrapper>
+		</React.Fragment>
+	)
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 
 import { Provider } from "react-redux"
 import { createStore, applyMiddleware, compose } from "redux"
 import reduxThunk from "redux-thunk"
+import reducers from "./reducers"
 
 import { ThemeProvider } from 'styled-components';
-
 import theme from './theme';
 import GlobalStyle from './globals';
 
@@ -35,8 +34,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
