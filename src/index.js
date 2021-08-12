63c8134d53c2b6f43d53d47331050e420f9b78f7
Evgeniy <nifpopnaf@gmail.com>
2021-08-12 16:32:02 +0400

    create redusers with base store

--- src/index.js
+++ src/index.js
@@ -2,22 +2,39 @@ import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware, compose } from "redux"
+import reduxThunk from "redux-thunk"
+
 import { ThemeProvider } from 'styled-components';
 
 import theme from './theme';
 import GlobalStyle from './globals';
 
+
+import App from './containers/App';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+	reducers,
+	composeEnhancers(applyMiddleware(reduxThunk))
+)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Fragment>
-        <App />
-        <GlobalStyle />
-      </Fragment>
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<Fragment>
+				<Provider store={store}>
+					<App />
+				</Provider>
+				
+				<GlobalStyle />
+			</Fragment>
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
